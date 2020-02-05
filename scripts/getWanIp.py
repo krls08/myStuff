e1329d50d74b2d26aece8cedea1abfec88cb69e8
krlspj <krls.pjds@gmail.com>
2020-02-05 23:47:23 +0100

    upd

--- scripts/getWanIp.py
+++ scripts/getWanIp.py
@@ -1,17 +1,35 @@
 import urllib.request
+import smtplib
+from email.mime.multipart import MIMEMulitipart
+from email.mime.text import MIMEText
 
-try:
-    f = open("extIp.txt","r")
-    myIp = f.read()
-    print("file found")
-except :
-    print('file doesnt exist -> create file')
+def writeIP():
     f = open("extIp.txt","w")
     externalIp = urllib.request.urlopen('https://api.ipify.org/').read().decode('utf8')
     f.write(externalIp)
+    f.close()
 
-f.close()
-externalIp = urllib.request.urlopen('https://api.ipify.org/').read().decode('utf8')
+def main():
+    try:
+        f = open("extIp.txt","r")
+        myIp = f.read()
+        print("file found")
+        f.close()
+    except :
+        print('file doesnt exist -> create file')
+        writeIP()
+        myIp = externalIp
 
-print('my external ip: ' + externalIp)
+    print('Compraing ips')
+    externalIp = urllib.request.urlopen('https://api.ipify.org/').read().decode('utf8')
+    if (externalIp == myIp):
+        print("Same ip, nothing to do")
+    else:
+        print("Ip changed\nSending new IP...")
+        writeIP()
 
+
+    print('my external ip: ' + externalIp)
+
+if __name__=="__main__":
+    main()
